64dbdecf03ffd7c109dff58629047905e4cc3625
YandyMC <yandymc016@gmail.com>
2022-10-09 19:14:34 -0500

    Añadir comentarios y modificar la estetica de la aplicacion

--- Dado_App/app/src/main/java/dado/app/MainActivity.kt
+++ Dado_App/app/src/main/java/dado/app/MainActivity.kt
@@ -3,22 +3,26 @@ package dado.app
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton: Button = findViewById(R.id.button)
+        // configurar el funcionamiento del boton
         rollButton.setOnClickListener {
+            //ejecuta la funcion rollDice
             rollDice()
         }
 
     }
     private fun rollDice() {
         val dice = Dice(6)
+        // selecciona el numero obtenido al tirar los dados
         val diceRoll = dice.roll()
+        //Encuentra la image a reempazar
         val diceImage: ImageView = findViewById(R.id.image1)
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
@@ -28,8 +32,13 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+        // realiza los respectivos cambios al dado
         diceImage.setImageResource(drawableResource)
+        // cambia el contenido de la descripcion de las imagenes
         diceImage.contentDescription = diceRoll.toString()
+        // envia un mesaje corto al usuario con el valor del dado
+        Toast.makeText(this, "El valor del datos es " + diceRoll, Toast.LENGTH_SHORT).show()
+
     }
 }
 
